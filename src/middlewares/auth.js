5af24e18b41c09b3825889870ff8980364e374ed
Sparsh Singh <sparshsingh03012003@gmail.com>
2025-02-01 05:46:00 +0530

    added env

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -18,7 +18,7 @@ const userAuth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (err) {
-    res.status(400).send("ERR:" + err.message);
+    res.status(400).send("Err:" + err.message);
   }
 };
 module.exports = { userAuth };
